da089edf8d4b3c540556ed073f907be3a433f36b
jnaoroji <jennynaoroji@gmail.com>
2023-05-01 11:55:43 +1000

    div order changed in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,12 @@
-// var GiphyApiKey: Tx6284aZTIMxVEBFzICg2PUFWP8R9FCu;
-// var MusicBrainzUrl:'https://musicbrainz.org/ws/2/';
-//  lookup:   /<ENTITY_TYPE>/<MBID>?inc=<INC>&fmt=json
-//  our look up: /<=ARTIST>/<MBID>?inc=<WORKS>&fmt=json
-//  browse:   /<RESULT_ENTITY_TYPE>?<BROWSING_ENTITY_TYPE>=<MBID>&limit=<LIMIT>&offset=<OFFSET>&inc=<INC>
-//  search:   /<ENTITY_TYPE>?query=<QUERY>&limit=<LIMIT>&offset=<OFFSET>
-// Ed Sheeran MBID = b8a7c51f-362c-4dcb-a259-bc6e0095f0a6
-// Adele MBID = cc2c9c3c-b7bc-4b8b-84d8-4fbd8779e493
-// Snoop Dog MBID = f90e8b26-9e52-4669-a5c9-e28529c47894
-// Eminem MBID = b95ce3ff-3d05-4e87-9e01-c97b66af13d4
-//"https://api.giphy.com/v1/stickers/translate?api_key=" + apiKey + "&s=" + searchTerm;
 
-//the button id that executes the game
+//declare all variables
 var artistButtonsEl = document.querySelectorAll('.box2');
 var beatlesButtonEl = document.querySelector('#beatlesbtn');
 var adeleButtonEl = document.querySelector('#adelebtn');
 var queenButtonEl = document.querySelector('#queenbtn');
 var michaelButtonEl = document.querySelector('#michaelbtn');
 
+//object of artist urls on music brainz
 const artistApiUrls = {
     "michaelbtn": "https://musicbrainz.org/ws/2/artist/2f9ecbed-27be-40e6-abca-6de49d50299e?inc=works&fmt=json",
     "queenbtn": "https://musicbrainz.org/ws/2/artist/0383dadf-2a4e-4d10-a46a-e9e041da8eb3?inc=works&fmt=json",
@@ -92,11 +82,7 @@ var displayGame = function (data) {
 
     });
     console.log(titles);
-
-    var RandomButton = document.createElement("button");
-        RandomButton.textContent = "Get Giphy!";
-        gameArea.appendChild(RandomButton);
-        RandomButton.addEventListener('click', createGiphyRequest);
+    createGiphyRequest();
 }
 
     var createGiphyRequest = function (){
@@ -136,12 +122,13 @@ var displayGif = function (data){
     gifEl.src = gif;
     gifEl.autoplay = true;
     gifEl.loop = true;
+    gifEl.style.maxHeight = "250px";
 
     gameArea.appendChild(gifEl);    
  
 }
 
-// buttonEl.addEventListener('click', playGame);
+
 artistButtonsEl.forEach(function(button) {
     button.addEventListener('click', playGame);
   });
